9f02c837a6d50280cad4e907c6af46b394911604
Oluwaseye Ayooluwa Philip <depaytez@gmail.com>
2025-07-30 01:33:14 +0100

    setup the index.tsx page

---
+++ alx-project-2/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const Home: React.FC = () => {
+  return(
+    <p>Welcome to my page</p>
+  );
+}
+
+export default Home;
