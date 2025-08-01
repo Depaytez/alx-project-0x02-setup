ce225e5724f8243002f278113c83d64f6f40d035
Oluwaseye Ayooluwa Philip <depaytez@gmail.com>
2025-08-02 00:24:26 +0100

    rush to debug

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -23,6 +23,7 @@ const Users: NextPage<{ users: UserCardProps[] }> = ({ users }) => {
  );
 };
 
+// getStaticProps() for fetching user data
 export const getStaticProps: GetStaticProps = async () => {
  const response = await fetch('https://jsonplaceholder.typicode.com/users');
  const users: UserCardProps[] = await response.json();
